f36a03c56a1158cfcc45dd97667de6dd7bd3f265
Mosharof Khan <mosharof5khan@gmail.com>
2023-11-26 02:12:15 +0600

    Home Screen UI update

--- lib/src/module/student/login&sign_up/local_widget/input_form.dart
+++ lib/src/module/student/login&sign_up/local_widget/input_form.dart
@@ -8,14 +8,13 @@ class InputField extends StatelessWidget {
   final String errorText;
   final TextEditingController controller;
   final IconButton? suffixIcon;
-  const InputField({
-    super.key,
-    required this.hintText,
-    required this.obsecureText,
-    required this.controller,
-    required this.errorText,
-    this.suffixIcon
-  });
+  const InputField(
+      {super.key,
+      required this.hintText,
+      required this.obsecureText,
+      required this.controller,
+      required this.errorText,
+      this.suffixIcon});
 
   @override
   Widget build(BuildContext context) {
@@ -28,23 +27,21 @@ class InputField extends StatelessWidget {
               hintText: hintText,
               enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(25.r),
-                  borderSide: BorderSide(color: CustomColor.blueGrey.withOpacity(.2), width: 1.w)
-              ),
+                  borderSide: BorderSide(
+                      color: CustomColor.blueGrey.withOpacity(.2), width: 1.w)),
               focusedBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(25.r),
-                  borderSide: BorderSide(color: CustomColor.lightTeal, width: 1.w)
-              ),
+                  borderSide:
+                      BorderSide(color: CustomColor.lightTeal, width: 1.w)),
               fillColor: CustomColor.lightTeal.withOpacity(0.1),
               filled: true,
-              suffixIcon: suffixIcon
-          ),
-          validator: (value){
-            if(value!.isEmpty){
+              suffixIcon: suffixIcon),
+          validator: (value) {
+            if (value!.isEmpty) {
               return errorText;
             }
             return null;
-          }
-      ),
+          }),
     );
   }
 }
