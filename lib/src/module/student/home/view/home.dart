e02327e6bd9160d7558d387175066566034f4afd
Mosharof Khan <mosharof5khan@gmail.com>
2023-11-26 23:28:19 +0600

    naveBar created

--- lib/src/module/student/home/view/home.dart
+++ lib/src/module/student/home/view/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:npi_project/src/module/student/home/local_widget/educataion_Info_card.dart';
 import 'package:npi_project/src/module/student/home/local_widget/greeting.dart';
+import 'package:npi_project/src/module/student/home/local_widget/nav_bar.dart';
 import 'package:npi_project/src/module/student/home/local_widget/occupation_info_card.dart';
 import 'package:npi_project/src/module/student/home/local_widget/personal_info_card.dart';
 import 'package:npi_project/src/module/student/home/local_widget/profile_indicator_part.dart';
@@ -19,11 +20,16 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+            elevation: 0,
+            iconTheme: const IconThemeData(color: Colors.black),
+            backgroundColor: Colors.white),
+        drawer: const NavBar(),
         body: Container(
           padding: EdgeInsets.all(24.w),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               // gradient: LinearGradient(
               //     begin: Alignment.topCenter,
               //     end: Alignment.bottomCenter,
@@ -32,8 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
               //       CustomColor.lightTeal.withOpacity(.5),
               //     ]
               // )
-          ),
-          child:SingleChildScrollView(
+              ),
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
